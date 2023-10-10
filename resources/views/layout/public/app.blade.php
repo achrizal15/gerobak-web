c7b423da4765d288ed8b907e36c33dcce47f32aa
RIZAL <achrizal15@gmail.com>
2023-10-10 23:29:31 +0700

    meta

--- resources/views/layout/public/app.blade.php
+++ resources/views/layout/public/app.blade.php
@@ -7,7 +7,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gerobak Web</title>
-    <link rel=icon href="{{ asset('dist/public') }}/assets/img/favicon.png" sizes="20x20" type="image/png">
+    <meta name="description"
+        content="Gerobak Web - Solusi Website untuk Bisnis Anda. Kami menyediakan layanan pembuatan website yang modern dan terjangkau.">
+    <meta name="keywords" content="Gerobak Web, website, bisnis, solusi, pembuatan website">
+    <meta name="author" content="Gerobak Web">
+
+    <!-- OG Meta Tags -->
+    <meta property="og:title" content="Gerobak Web - Solusi Website untuk Bisnis Anda">
+    <meta property="og:description"
+        content="Gerobak Web - Solusi Website untuk Bisnis Anda. Kami menyediakan layanan pembuatan website yang modern dan terjangkau.">
+    <meta property="og:image" content="{{ Vite::asset('resources/images/logo/logo.png') }}">
+    <meta property="og:url" content="https://gerobakweb.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Gerobak Web">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="{{ Vite::asset('resources/images/logo/logo.png') }}">
+    <meta name="twitter:title" content="Gerobak Web - Solusi Website untuk Bisnis Anda">
+    <meta name="twitter:description"
+        content="Gerobak Web - Solusi Website untuk Bisnis Anda. Kami menyediakan layanan pembuatan website yang modern dan terjangkau.">
+    <meta name="twitter:image" content="{{ Vite::asset('resources/images/logo/logo.png') }}">
+    <meta name="twitter:url" content="{{ Vite::asset('resources/images/logo/logo.png') }}">
+    <meta name="twitter:site" content="@gerobakweb">
+    <link rel=icon href="{{ Vite::asset('resources/images/logo/logo.png') }}" sizes="20x20" type="image/png">
 
     <!-- Stylesheet -->
     <link rel="stylesheet" href="{{ asset('dist/public') }}/assets/css/bootstrap.min.css">
