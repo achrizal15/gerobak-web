e7af9b3c7cb813113f55eef47ade42bff3973cf0
Junaidy Susilo <junaidysusilox@gmail.com>
2023-10-12 15:30:08 +0700

    update footer

---
+++ resources/views/layout/public/footer.blade.php
@@ -0,0 +1,91 @@
+<!-- footer area start -->
+<footer class="footer-area footer-area-2 bg-gray mt-0 pd-top-120">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 mb-3 mb-md-0 col-md-6">
+                <img src="{{ Vite::asset('resources/images/logo/logo.png') }}" alt="img" width="200px" height="84px">
+            </div>
+            <div class="col-lg-3 col-md-6 ps-xl-5">
+                <div class="widget widget_nav_menu">
+                    <h4 class="widget-title">@lang('public.footer.advanced_service.title')</h4>
+                    <ul>
+                        <li><a href="https://www.rengganikaryasemesta.com/"><i
+                                    class="fas fa-chevron-right"></i>@lang('public.footer.advanced_service.ui_design')</a></li>
+                        <li><a href="https://www.rengganikaryasemesta.com/"><i
+                                    class="fas fa-chevron-right"></i>@lang('public.footer.advanced_service.web_design')</a></li>
+                        <li><a href="https://www.rengganikaryasemesta.com/"><i
+                                    class="fas fa-chevron-right"></i>@lang('public.footer.advanced_service.digital_marketing')</a></li>
+                        <li><a href="https://www.rengganikaryasemesta.com/"><i
+                                    class="fas fa-chevron-right"></i>@lang('public.footer.advanced_service.web_development')</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 ps-xl-5">
+                <div class="widget widget_nav_menu">
+                    <h4 class="widget-title">GerobakWeb</h4>
+                    <ul>
+                        <li><a href="#home"><i class="fas fa-chevron-right"></i> @lang('public.navbar.home')</a></li>
+                        <li><a href="#what-you-get-area"><i class="fas fa-chevron-right"></i> @lang('public.navbar.what_you_get')</a></li>
+                        <li><a href="#business-lines-area"><i class="fas fa-chevron-right"></i> @lang('public.navbar.business_lines') </a>
+                        </li>
+                        <li><a href="/#price-list-area"><i class="fas fa-chevron-right"></i> @lang('public.navbar.price_list')</a></li>
+                        <li><a href="/#our-friend-area"><i class="fas fa-chevron-right"></i>@lang('public.navbar.our_friend')</a></li>
+                        <li><a href="/#how-we-work-area"><i class="fas fa-chevron-right"></i> @lang('public.navbar.how_we_work')</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="widget widget-recent-post">
+                    <h4 class="widget-title">@lang('public.footer.contact_us')</h4>
+                    <div class="widget widget_contact">
+                        <ul class="details">
+                            <li><i class="fa fa-map-marker-alt"></i>Jl. Kemiren, Dusun Kedaleman, Kemiren, Kec. Glagah,
+                                Kabupaten Banyuwangi, Jawa Timur 68432
+                            </li>
+                            <li class="mt-3"><i class="fa fa-phone-alt"></i> (+62) 89512589756</li>
+                            <li class="mt-2"><i class="fas fa-envelope"></i> info@gerobakweb.com</li>
+                        </ul>
+                        <ul class="social-media mt-4">
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://wa.me/6289512589756" target="_blank">
+                                    <i class="fab fa-whatsapp"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-youtube"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 align-self-center">
+                    <a href="https://www.rengganikaryasemesta.com/">
+                        <p>© Renggani Karya Semesta 2023 </p>
+                    </a>
+                </div>
+                <div class="col-md-6 text-lg-end">
+                    <a href="#">Terms & Condition</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer area end -->
