ebf60f6288677dd72d4c9a6efadd904aac8a5b56
RIZAL <achrizal15@gmail.com>
2023-10-12 16:43:22 +0700

    google verify

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/google126bc52d42430a47.html', function () {
+         return view('google126bc52d42430a47');
+});
